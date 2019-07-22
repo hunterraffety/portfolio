1f120b2169f472783bdf01fa14ec6037d78e0410
hsrsbd <hsraffety@gmail.com>
2019-07-22 16:15:28 -0700

    update navbar.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -25,7 +25,7 @@ export default class Navigation extends React.Component {
 
   render() {
     return (
-      <Navbar light expand='md'>
+      <Navbar light expand='md' fixed='top'>
         <NavItem className='navItem'>
           Hunter Raffety | Full Stack Web Developer
         </NavItem>
