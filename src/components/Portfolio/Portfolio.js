0fa419496c63f007e12c531e70309eb35dd3f9ef
hsrsbd <hsraffety@gmail.com>
2019-07-22 20:32:04 -0700

    updating layout and content.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,60 +14,60 @@ import {
 
 // components
 
-export default function Portfolio() {
+const Portfolio = props => {
   return (
-    <div
-      className='portfolio-container d-flex align-items-center'
-      id='portfolio'
-    >
+    <div className='portfolio-container align-items-center' id='portfolio'>
       <Row className='justify-content-md-center portfolio-content'>
         <Col md='3'>
-          <Card>
+          <Card className='card-top'>
             <CardImg
               top
               width='100%'
               src='http://lorempixel.com/640/480/technics'
               alt='Card image cap'
             />
-            <CardBody className='about-card-body'>
-              <CardText>Here is something you can see.</CardText>
-            </CardBody>
           </Card>
         </Col>
         <Col md='3'>
-          <Card>
+          <Card className='card-top'>
             <CardImg
               top
               width='100%'
               src='http://lorempixel.com/640/480/abstract'
               alt='Card image cap'
             />
-            <CardBody className='about-card-body'>
-              <CardText>
-                Did you see the first one? If not, here's another.
-              </CardText>
-            </CardBody>
           </Card>
         </Col>
         <Col md='3'>
-          <Card>
+          <Card className='card-top'>
             <CardImg
               top
               width='100%'
               src='http://lorempixel.com/640/480/city'
               alt='Card image cap'
             />
-            <CardBody className='about-card-body'>
-              <CardText>
-                Everybody loves Lorem Ipsum. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Laboriosam velit consectetur ullam
-                nobis, vel recusandae tempora dignissimos molestias ad
-                doloremque quae doloribus quia, pariatur optio.
-              </CardText>
-            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      <Row className='justify-content-md-center portfolio-desc'>
+        <Col md='3'>
+          <Card>
+            <CardText>asd</CardText>
+          </Card>
+        </Col>
+        <Col md='3'>
+          <Card>
+            <CardText>asd</CardText>
+          </Card>
+        </Col>
+        <Col md='3'>
+          <Card>
+            <CardText>asd</CardText>
           </Card>
         </Col>
       </Row>
     </div>
   );
-}
+};
+
+export default Portfolio;
