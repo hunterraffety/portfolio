14ad6cba1966df4306784e997b806be96b10ae4c
hsrsbd <hsraffety@gmail.com>
2019-07-23 14:56:49 -0700

    updating form.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,7 +18,7 @@ class Footer extends React.Component {
           </Col>
         </Row>
         <Row>
-          <Form className='pad bottom-line'>
+          <Form className='pad bottom-line' netlify>
             <Row form>
               <Col md={6}>
                 <FormGroup>
