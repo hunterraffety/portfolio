dedd547b6e461d929219dd79a1d380982300de60
Hunter Raffety <26361513+hunterraffety@users.noreply.github.com>
2024-09-29 10:04:55 -0400

    updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -25,38 +25,48 @@ export default function AboutPage() {
             <div className={styles.inspirationalBlock}>
               <h4 className={styles.inspirationalTitle}>Philosophy</h4>
               <p>
-                I woke up today a better person than when I woke up yesterday.
-                That doesn&apos;t mean that I don&apos;t have a relentless drive
-                to become better today though. I am pretty great, but I know I
-                can do more. It makes sense to me that if I can do amazing
-                things in life, I&apos;m just going to go ahead and do those
-                things.
+                I woke up today hopefully a little bit better of a person than
+                yesterday. Having said that however, it doesn&apos;t mean that I
+                don&apos;t have a relentless drive to become better today.
+                I&apos;ve been able to do and see and build some neat things in
+                life professionally and personally, but I know I can do more. It
+                makes sense to me that if I can do amazing things in life,
+                I&apos;m just going to go ahead and do those things and surround
+                myself with the best people possible to do it with
               </p>
-              <h4 className={styles.inspirationalTitle}>Relentless</h4>
             </div>
 
             <div className={styles.inspirationalBlock}>
+              <h4 className={styles.inspirationalTitle}>Relentless</h4>
               <p>
                 I decided a long time ago that I can&apos;t allow barriers
                 prevent me from being successful. I&apos;m going to make use of
                 every skill and resource that I have along with all of the
                 things I have learned during my life to find success. I&apos;ll
                 also look to others around me when I need help and support.
-                It&apos;s okay to mindfully ask for help from time to time.
+                It&apos;s okay to mindfully ask for help from time to time
               </p>
-              <h4 className={styles.inspirationalTitle}>
-                A Champion of Others
-              </h4>
             </div>
 
             <div className={styles.inspirationalBlock}>
+              <h4 className={styles.inspirationalTitle}>
+                A Champion of Others
+              </h4>
               <p>
-                Nobody in life achieves anything great without the help of
-                someone else. I know that I haven&apos;t. I have people who are
-                invested in me and believe in me. I was a Special Education
-                teacher and co-taught secondary and high school mathematics -- I
-                gave my all every day. I want to give back to others in any way
-                I am capable of.
+                I pretty firmly believe that very few in this world are able to
+                achieve greatness or great things without the help of others. I
+                know that I haven&apos;t. Luckily however I have had (have)
+                people in my life who are invested in me and believe in me. It
+                is fair to reason then that because I have had such fortune in
+                my life that I should continue to turn that outward using the
+                skills and resources that I have. At one point in my life it
+                looked like being a special education classroom teacher (for
+                over a decade!) and now it looks like building tools for people
+                to get things done and to be part of great things, still. If
+                I&apos;m able to proliferate neat things and excitement into the
+                world through creativity, a passion for growth, and choosing to
+                be fascinated, then it only makes sense to help others do the
+                same, because it&apos;s pretty amazing.
               </p>
             </div>
           </div>
