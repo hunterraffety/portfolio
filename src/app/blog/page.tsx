8366a765e48c49e794bd6d607b5bb348b813c203
Hunter Raffety <26361513+hunterraffety@users.noreply.github.com>
2024-09-30 21:06:41 -0400

    updates

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -25,16 +25,21 @@ export default function BlogPage() {
         {/* <FaSearch className={styles.icon} /> */}{' '}
         {/* If using React Icons */}
       </div>
-      <h2 className={styles.title}>Blog Coming Soon</h2>
+      <h2 className={styles.title}>
+        Blog Coming Soon ... but in the meantime!
+      </h2>
       <p className={styles.subtitle}>
-        I&apos;m crafting something special! (Building a Roubo
-        workbench in my wood shop! I also just got back from traveling (9/24))
+        I&apos;m crafting something special! (Building a Roubo workbench in my
+        wood shop! I also just got back from traveling (9/24))
         <br />
         <br />
         (Legit! This is my travel and woodworking blog, not for professional
         purposes, but you can look too!~)
         <Link href='/europe'>
-          <p>Europe (Denmark, Italy, Austria, Germany, France, Switzerland) 2024 (yay!)</p>
+          <p>
+            Europe (Denmark, Italy, Austria, Germany, France, Switzerland) 2024
+            (yay!)
+          </p>
         </Link>
         <Link href='/pets'>
           <p>Watson & Sherlock</p>
