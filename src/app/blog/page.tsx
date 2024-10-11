4974abd5e8e543770d62bf6e78d490728323e82c
Hunter Raffety <26361513+hunterraffety@users.noreply.github.com>
2024-10-11 07:38:07 -0400

    updates

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,10 @@
 import styles from './Blog.module.scss'
-import MagnifyingGlass from '../components/icons/MagnifyingGlass'
 import Link from 'next/link'
 
 export default function BlogPage() {
   return (
     <section className={styles.placeholder}>
-      <div className={styles.iconContainer}>
-        {/* <MagnifyingGlass className={styles.icon} /> */}
-      </div>
-      {/* <h2 className={styles.title}>
-        Learn more about my travels and experiences.
-      </h2> */}
+      <div className={styles.iconContainer}></div>
       <p className={styles.subtitle}>
         <Link href='/europe'>
           <p>Europe 2024</p>
